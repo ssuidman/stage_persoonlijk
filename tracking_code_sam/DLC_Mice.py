cf5248b9d98cbe3ac36d47d5de6ced6a8df82f52
Sam <ssuidman@gmail.com>
2020-03-10 17:34:34 +0100

    There is a first example plot of the likelihoods of different bodyparts above eachother, but still without the eye camera data with it. So 15 (broken) lines above eachother with a legend.

--- tracking_code_sam/DLC_Mice.py
+++ tracking_code_sam/DLC_Mice.py
@@ -135,13 +135,34 @@ for j in M3728_alone_cam5_mice_low_likelihood_reduced_frames_likelihood: #look a
 
 
 
+M3728_alone_cam5_mice_likelihood_binary = [] #Making a list for all bodyparts (len(list)=15) where if p<0.99 the value of list[i] becomes NaN and otherwise becomes 1+i (so that the lines are above eachother)
+k=0 #place for the first line
+for i in M3728_alone_cam5_mice_likelihood_columns: #going through each bodypart
+    M3728_alone_cam5_mice_likelihood_binary_column = [] #making a list per bodypart where [1,NaN,NaN,1,1,...]
+    for j in M3728_alone_cam5_mice_likelihood[i]: #cloning the likelihood list of a bodypart [0.3338,0.9783,...]
+        M3728_alone_cam5_mice_likelihood_binary_column.append(j) # " " "
+    M3728_alone_cam5_mice_likelihood_binary_column = np.asarray(M3728_alone_cam5_mice_likelihood_binary_column) #making type=array of it
+    M3728_alone_cam5_mice_likelihood_binary_column[M3728_alone_cam5_mice_likelihood_binary_column<likelihood_threshold] = np.NaN #setting p<0.99 to not a number
+    M3728_alone_cam5_mice_likelihood_binary_column[M3728_alone_cam5_mice_likelihood_binary_column>likelihood_threshold] = len(M3728_alone_cam5_mice_likelihood_columns)-k #setting p>0.99 to 15 (or 14,13,...,2,1) so that all lines can be plotted above eachother)
+    M3728_alone_cam5_mice_likelihood_binary.append(M3728_alone_cam5_mice_likelihood_binary_column) #adding the list of one bodypart to the bigger list
+    k+=1 #setting k+1 so that the next bodypart comes at the line above/below the other
+
+for j in range(len(M3728_alone_cam5_mice_likelihood_columns)): #going through each body part
+    plt.plot(M3728_alone_cam5_mice_likelihood_binary[j], label="{}".format(M3728_alone_cam5_mice_likelihood_columns[j][1])) #plot the line of the binary likelihood
+    plt.legend() #making the legend
+plt.savefig('/Users/samsuidman/Desktop/likelihood_figures/plaatje.png',dpi=1200) #saving the picture at high quality
+
+
+
+
+
+
 
 t2 = time.time()
 
-print('tijd die het heeft geduurd is {}'.format(t2-t1))
+print('The time it took for running is {}'.format(t2-t1))
 
 
-#the script works if I enter everything above here once
-
+#the script works if I enter everything above here at once (and takes about 63 seconds to run)
 
 
